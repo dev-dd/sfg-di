b8d1b17c1da0632f48c51e66ac9e771d254dc1f7
dev-dd <dv.dt@hotmail.com>
2020-07-16 16:06:42 +0200

    Add qualifiers to controllers

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    @Autowired // optional with constructor injection
+    public ConstructorInjectedController(@Qualifier("constructorInjectedGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
